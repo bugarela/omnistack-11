00d226280afa98c70c8ef1ba05d2d5d1e930fef6
Gabriela Mafra <gabrielamoreiramafra@gmail.com>
2020-03-25 01:59:58 -0300

    Add routes and controllers

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+
+const connection = require('../database/connection.js');
+
+module.exports = {
+    async create(request, response) {
+        const { id } = request.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('Name')
+            .first();
+
+        if (!ong) {
+            response.status(400).json({ error: 'ONG not found'});
+        }
+
+        return response.json({ ong });
+    },
+
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*');
+
+        return response.json(ongs);
+    }
+};
+
+// routes.get('/ongs', async (request, response) => {
+//     const ongs = await connection('ongs').select('*');
+
+//     return response.json(ongs);
+// });
+
