305aaaf906e4ff43193dfac8a506b9e561ab8a1f
Gabriela Mafra <gabrielamoreiramafra@gmail.com>
2020-03-27 01:56:00 -0300

    Add mobile app with expo

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+    marginTop: 48,
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
+
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380',
+  },
+
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+  },
+
+  heroTitle: {
+    fontSize: 20,
+    lineHeight: 30,
+    color: '#13131a',
+    fontWeight: 'bold',
+  },
+
+  heroDescription: {
+    fontSize: 15,
+    marginTop: 16,
+    color: '#737380',
+  },
+
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+  },
+
+  action: {
+    backgroundColor: '#e02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  actionText: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
+});
